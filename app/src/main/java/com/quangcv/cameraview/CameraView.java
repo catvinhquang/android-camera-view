802d1bc9bcc72ed06124505010a28ce111a1a536
Quang Cat <catvinhquang@gmail.com>
2019-10-29 16:26:58 +0700

    clean code

--- app/src/main/java/com/quangcv/cameraview/CameraView.java
+++ app/src/main/java/com/quangcv/cameraview/CameraView.java
@@ -1,17 +1,26 @@
 package com.quangcv.cameraview;
 
 import android.content.Context;
+import android.hardware.Camera;
 import android.support.annotation.NonNull;
 import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import static java.lang.Math.abs;
+
 public class CameraView extends SurfaceView implements SurfaceHolder.Callback {
 
-    private Camera1Wrapper impl;
-    private int surfaceWidth;
-    private int surfaceHeight;
+    private boolean isReady = false;
+    private boolean isTakingPicture = false;
+    private Camera camera;
+    private CameraCallback callback;
 
     public CameraView(Context context) {
         this(context, null);
@@ -29,8 +38,6 @@ public class CameraView extends SurfaceView implements SurfaceHolder.Callback {
         SurfaceHolder holder = getHolder();
         holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
         holder.addCallback(this);
-
-        impl = new Camera1Wrapper(this);
     }
 
     @Override
@@ -40,41 +47,123 @@ public class CameraView extends SurfaceView implements SurfaceHolder.Callback {
 
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-        surfaceWidth = width;
-        surfaceHeight = height;
+        isReady = width != 0 && height != 0;
         if (!ViewCompat.isInLayout(CameraView.this)) {
-            impl.onSurfaceChanged();
+            startPreview();
         }
     }
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
-        surfaceWidth = surfaceHeight = 0;
+        isReady = false;
     }
 
     public void start() {
-        if (!impl.start()) {
-            CameraCallback c = impl.getCallback();
-            impl = new Camera1Wrapper(this);
-            impl.setCallback(c);
-            impl.start();
+        if (camera == null) {
+            camera = Camera.open(0);
+            callback.onCameraOpened();
+            startPreview();
+        }
+    }
+
+    private void startPreview() {
+        if (camera != null && isReady) {
+            try {
+                Camera.Parameters params = camera.getParameters();
+
+                Camera.Size preSize = getPreviewSize(params.getSupportedPreviewSizes(),
+                        getWidth(),
+                        getHeight());
+                params.setPreviewSize(preSize.width, preSize.height);
+
+                // output size
+                Camera.Size picSize = getPictureSize(params.getSupportedPictureSizes(),
+                        preSize.width,
+                        preSize.height);
+                params.setPictureSize(picSize.width, picSize.height);
+
+                // TODO quang: check focus feature is available
+                params.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE); // Auto focus
+
+                // TODO quang: rotation formular
+                params.setRotation(90); // Để xuất hình portrait
+                camera.setDisplayOrientation(90); // Portrait preview
+
+                camera.setParameters(params);
+                camera.setPreviewDisplay(getHolder());
+                camera.startPreview();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 
     public void stop() {
-        impl.stop();
+        if (camera != null) {
+            camera.stopPreview();
+        }
+        release();
     }
 
     public void takePicture() {
-        impl.takePicture();
+        if (camera != null && !isTakingPicture) {
+            isTakingPicture = true;
+            camera.takePicture(null, null, null, new Camera.PictureCallback() {
+                @Override
+                public void onPictureTaken(byte[] data, Camera camera) {
+                    isTakingPicture = false;
+                    callback.onPictureTaken(data);
+                    camera.cancelAutoFocus();
+                    camera.startPreview();
+                }
+            });
+        }
+    }
+
+    private Camera.Size getPreviewSize(List<Camera.Size> supportedSizes,
+                                       final int viewWidth,
+                                       final int viewHeight) {
+        return Collections.min(supportedSizes, new Comparator<Camera.Size>() {
+            @Override
+            public int compare(Camera.Size l, Camera.Size r) {
+                int diff1 = abs(viewWidth - l.width) + abs(viewHeight - l.height);
+                int diff2 = abs(viewWidth - r.width) + abs(viewHeight - r.height);
+                return diff1 - diff2;
+            }
+        });
+    }
+
+    private Camera.Size getPictureSize(List<Camera.Size> supportedSizes,
+                                       final int previewWidth,
+                                       final int previewHeight) {
+        List<Camera.Size> filtered = new ArrayList<>();
+        double previewRatio = previewWidth / (double) previewHeight;
+        for (Camera.Size s : supportedSizes) {
+            double itemRatio = s.width / (double) s.height;
+            if (itemRatio == previewRatio) {
+                filtered.add(s);
+            }
+        }
+        return Collections.max(filtered, new Comparator<Camera.Size>() {
+            @Override
+            public int compare(Camera.Size l, Camera.Size r) {
+                int diff1 = abs(previewWidth - l.width) + abs(previewHeight - l.height);
+                int diff2 = abs(previewWidth - r.width) + abs(previewHeight - r.height);
+                return diff1 - diff2;
+            }
+        });
+    }
+
+    private void release() {
+        if (camera != null) {
+            camera.release();
+            camera = null;
+            callback.onCameraClosed();
+        }
     }
 
     public void setCallback(@NonNull CameraCallback callback) {
-        impl.setCallback(callback);
-    }
-
-    public boolean isReady() {
-        return surfaceWidth != 0 && surfaceHeight != 0;
+        this.callback = callback;
     }
 
 }
